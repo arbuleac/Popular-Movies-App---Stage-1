96475711567b03a7bb50c47face5520f2f7164a6
arbuleac <dev@arbuleac.com>
2015-07-18 16:05:59 +0300

    removed API_KEY

--- app/src/main/java/com/arbuleac/movieapp/util/Constants.java
+++ app/src/main/java/com/arbuleac/movieapp/util/Constants.java
@@ -4,7 +4,7 @@ package com.arbuleac.movieapp.util;
  * @since 7/12/15.
  */
 public class Constants {
-    public static final String API_KEY = "f2aecc06020a88bcf2d18d0f4979ae74";
+    public static final String API_KEY = "ENTER YOUR KEY HERE";
     public static final String SORT_BY_POPULARITY = "popularity.desc";
     public static final String SORT_BY_HIGHEST_RATED = "vote_average.desc";
     public static final String STATIC_IMAGE_ENDPOINT = "http://image.tmdb.org/t/p/";
